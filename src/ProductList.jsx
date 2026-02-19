8789bf959978e520f2cf4d6faafa3efd57737527
Beatriz Rosas <b.rosas@campus.fct.unl.pt>
2026-02-19 12:51:01 +0000

    Fixed UI.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -243,9 +243,9 @@ function ProductList({ onHomeClick }) {
     }
     const styleObjUl = {
         display: 'flex',
-        justifyContent: 'space-between',
+        // justifyContent: 'space-between',
         alignItems: 'center',
-        width: '1100px',
+        // width: '1100px',
     }
     const styleA = {
         color: 'white',
@@ -285,30 +285,27 @@ function ProductList({ onHomeClick }) {
                             </div>
                         </a>
                     </div>
-
                 </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div>
-                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
-                            <div style={{ position: "relative", display: "inline-block" }}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
-                                    <rect width="156" height="156" fill="none"></rect>
-                                    <circle cx="80" cy="216" r="12"></circle>
-                                    <circle cx="184" cy="216" r="12"></circle>
-                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
-                                </svg>
-                                <span style={{ position: "absolute", top: "8px", right: "8px", color: "white", fontSize: "20px", fontWeight: "bold", borderRadius: "50%", padding: "4px 8px", minWidth: "20px", textAlign: "center" }}>{calculateTotalQuantity()}</span>
-                            </div>
-                        </a>
-                    </div>
+                <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div>
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <div style={{ position: "relative", display: "inline-block" }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
+                            </svg>
+                            <span style={{ position: "absolute", top: "8px", right: "8px", color: "white", fontSize: "20px", fontWeight: "bold", borderRadius: "50%", padding: "4px 8px", minWidth: "20px", textAlign: "center" }}>{calculateTotalQuantity()}</span>
+                        </div>
+                    </a>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                         <div key={index}> {/* Unique key for each category div */}
-                            <h1>
+                            <h1 style={{ textAlign: 'center', margin: '20px 0' }}>
                                 <div>{category.category}</div> {/* Display the category name */}
                             </h1>
                             <div className="product-list"> {/* Container for the list of plant cards */}
